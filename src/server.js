80f0c527de9473b86e3ec192527ade5740cf3a52
vikram305 <mauryavikram305@gmail.com>
2020-11-25 10:22:09 +0530

    Installed Express and DotEnv and setup basic server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv')
+
+if(process.env.NODE_ENV !== 'production'){
+    dotenv.config({ path: './config/config.env' })
+    console.log(`port from config file : ${process.env.PORT}`)
+}
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
