ae136404fd53bc6fdc9e4f58b2a31aac575c62da
vikram305 <mauryavikram305@gmail.com>
2021-01-06 17:31:24 +0530

    Setup mongodb and mongoose

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./db/db')
+const colors = require('colors')
 
 // Route Files
 const blogs = require('./routes/blogs')
 
+// Load env vars
 if(process.env.NODE_ENV !== 'production'){
     dotenv.config({ path: './config/config.env' })
     console.log(`port from config file : ${process.env.PORT}`)
 }
+
+// Connect to database
+connectDB()
 
 const app = express()
 
@@ -23,7 +29,15 @@ app.use('/api/v1/blogs',blogs)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
