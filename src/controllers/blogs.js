f62440d8916ec786048f3655330fe0eff5ca4c9f
vikram305 <mauryavikram305@gmail.com>
2021-01-07 14:29:47 +0530

    Added Async Middleware

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,64 +1,49 @@
 const Blog = require('../models/Blog')
+const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
 
 //@desc     Get all blogs
 //@route    GET /api/v1/blogs
 //@access   Public
-exports.getBlogs = async (req,res,next) => {
-    try{
-        const blogs = await Blog.find()
-
-        res.status(200).json({ success: true, count: blogs.length, data: blogs })
-    } catch(error){
-        // res.status(400).json({ success: false })
-        next(error)
-    }
-}
+exports.getBlogs = asyncHandler(async (req,res,next) => {
+    const blogs = await Blog.find()
+    res.status(200).json({ success: true, count: blogs.length, data: blogs })
+})
 
 //@desc     Get single blogs
 //@route    GET /api/v1/blogs/:id
 //@access   Public
-exports.getBlog = async (req,res,next) => {
-    try{
-        const blog = await Blog.findById(req.params.id)
+exports.getBlog = asyncHandler(async (req,res,next) => {
+    
+    const blog = await Blog.findById(req.params.id)
 
-        if(!blog){
-            // return res.status(400).json({ success: false })
-            return next(new ErrorResponse(`Blog not found with id of ${req.params.id}`,404))
-        }
-
-        res.status(200).json({ success: true, data: blog })
-    } catch(error) {
-        // res.status(400).json({ success: false })
-        next(error)
-        // next(new ErrorResponse(`Blog not found with id of ${req.params.id}`,404))
+    if(!blog){
+        // return res.status(400).json({ success: false })
+         return next(new ErrorResponse(`Blog not found with id of ${req.params.id}`,404))
     }
-}
+
+    res.status(200).json({ success: true, data: blog })
+        
+})
 
 //@desc     Create new blog
 //@route    POST /api/v1/blogs
 //@access   Private
-exports.createBlog = async (req,res,next) => {
+exports.createBlog = asyncHandler(async (req,res,next) => {
 
-    try{
-        const blog = await Blog.create(req.body)
-        res.status(201).json({
-            success: true,
-            data: blog
-        })
-    } catch(error){
-        // res.status(400).json({ success: false })
-        next(error)
-    }
-   
-}
+    const blog = await Blog.create(req.body)
+    res.status(201).json({
+        success: true,
+        data: blog
+    })
+
+})
 
 //@desc     Update blog
 //@route    PUT /api/v1/blogs/:id
 //@access   Private
-exports.updateBlog = async (req,res,next) => {
+exports.updateBlog = asyncHandler(async (req,res,next) => {
 
-    try{
         const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -70,18 +55,14 @@ exports.updateBlog = async (req,res,next) => {
         }
     
         res.status(200).json({ success: true, data: blog })
-    } catch(error){
-        // res.status(400).json({ success: false })
-        next(error)
-    }
     
-}
+})
 
 //@desc     Delete blog
 //@route    DELETE /api/v1/blogs/:id
 //@access   Private
-exports.deleteBlog = async (req,res,next) => {
-    try{
+exports.deleteBlog = asyncHandler(async (req,res,next) => {
+    
         const blog = await Blog.findByIdAndDelete(req.params.id)
     
         if(!blog){
@@ -90,8 +71,5 @@ exports.deleteBlog = async (req,res,next) => {
         }
     
         res.status(200).json({ success: true, data: {} })
-    } catch(error){
-        // res.status(400).json({ success: false })
-        next(error)
-    }
-} 
+
+})
