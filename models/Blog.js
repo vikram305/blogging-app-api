e6fb2f999f63ec5e9ce13f4b9b810de5ff286879
vikram305 <mauryavikram305@gmail.com>
2021-01-07 09:42:59 +0530

    Creating Blog model

---
+++ models/Blog.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+ 
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        unique: false,
+        trim: true,
+        maxlength: [100, 'Title can not be more than 100 characters']
+    },
+
+    slug: String,
+    body: {
+        type: String,
+        required: [true, 'Please add a body'],
+        unique: false,
+        trim: true,
+        maxlength: [500, 'Body can not be more than 500 characters']
+    },
+
+    image: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Blog',BlogSchema)
