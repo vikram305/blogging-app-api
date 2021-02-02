81f841ef149a7a5390213eb225e65f8fb05e6539
vikram305 <mauryavikram305@gmail.com>
2021-02-02 09:51:38 +0530

    Encrypted User Password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,5 +6,14 @@ const ErrorResponse = require('../utils/errorResponse')
 //@route    GET /api/v1/auth/register
 //@access   Public
 exports.register = asyncHandler(async (req, res, next) => {
+    const {name, email, password} = req.body
+
+    //Create User
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    
     res.status(200).json({ success: true})
 })
